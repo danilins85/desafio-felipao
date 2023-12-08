deaf8b99ff3f5df650ec44553c64667ecd5f34de
danilins85 <danielalinsflores@yahoo.com.br>
2023-12-08 18:48:44 -0300

    outro teste

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-let nome = "felipe";
-console.log(nome);
+let nomeHeroi = "Novo Heroi";
+let XP = 8.000
+let nivelHeroi
 
+switch (true)
+{
+    case XP >= 1.000:
+        nivelHeroi = "Ferro";
+        break;
+    case XP <= 2.001:
+        nivelHeroi = "Bronze";
+        break;
+    case XP <= 5.001:
+        nivelHeroi = "Prata;";
+        break;
+    case XP <=6.001:
+        nivelHeroi = "Ouro";
+        break;
+    case XP <= 7.001:
+        nivelHeroi = "Platina";
+        break;
+    case XP <= 8.001:
+        nivelHeroi = "Asecendente";
+        break;
+    case XP <= 9.001:
+        nivelHeroi = "Imortal";
+        break;
+   default:
+        nivelHeroi = "Radiante"
+        break;  
+
+}
+
+XP = 8.000;
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)

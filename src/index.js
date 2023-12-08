2b549f4f043a90d507a0946b12a892b22333bd9e
danilins85 <danielalinsflores@yahoo.com.br>
2023-12-08 18:55:37 -0300

    Terminando o desafio

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
 let nomeHeroi = "Novo Heroi";
-let XP = 8.000
+let XP = 8000
 let nivelHeroi
 
-switch (true)
-{
-    case XP >= 1.000:
+switch (true) {
+    case XP < 1000:
         nivelHeroi = "Ferro";
         break;
-    case XP <= 2.001:
+    case XP >= 1001 && XP <= 2000:
         nivelHeroi = "Bronze";
         break;
-    case XP <= 5.001:
-        nivelHeroi = "Prata;";
+    case XP >= 2001 && XP <= 5000:
+        nivelHeroi = "Prata";
         break;
-    case XP <=6.001:
+    case XP >= 5001 && XP <= 7000:
         nivelHeroi = "Ouro";
         break;
-    case XP <= 7.001:
+    case XP >= 7001 && XP <= 8000:
         nivelHeroi = "Platina";
         break;
-    case XP <= 8.001:
-        nivelHeroi = "Asecendente";
+    case XP >= 8001 && XP <= 9000:
+        nivelHeroi = "Ascendente";
         break;
-    case XP <= 9.001:
+    case XP >= 9001 && XP <= 10000:
         nivelHeroi = "Imortal";
         break;
-   default:
-        nivelHeroi = "Radiante"
-        break;  
+    default:
+        nivelHeroi = "Radiante";  
 
 }
 
-XP = 8.000;
+XP = 8000;
 
 console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
